57b405023bfd4816479ed6e5ed2a38f793c517ba
Jeremy Herzog <jeremyaherzog@gmail.com>
2025-03-11 15:06:37 -0400

    feat: add ollama model interface

--- tagger/core/models/ollama.py
+++ tagger/core/models/ollama.py
@@ -14,9 +14,10 @@ from tagger.core.models.interface import (
 
 DEFAULT_MODEL = "ollama/llama3.2-vision:11b"
 
+
 class VisionOllama(VisionModel):
     model = DEFAULT_MODEL
-    
+
     def __init__(self, api_base: str = "http://localhost:11434"):
         self.api_base = api_base
 
@@ -37,7 +38,7 @@ class VisionOllama(VisionModel):
                 messages_for_completion.append(
                     {"role": message.role, "content": message.content}
                 )
-                
+
         print("MODEL:", self.model)
 
         result = completion(
@@ -48,11 +49,14 @@ class VisionOllama(VisionModel):
 
         return result.choices[0].message.content
 
+
 class Llama3211BVisionOllama(VisionOllama):
     model = "ollama/llama3.2-vision:11b"
 
+
 class Llava34BVisionOllama(VisionOllama):
     model = "ollama/llava:34b"
+
 
 class Phi4MiniJSONOutputOllama(JSONOutputTextModel):
     def __init__(self, api_base: str = "http://localhost:11434"):
