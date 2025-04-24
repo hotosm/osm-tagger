977412a91246ec591e3283da125b8cb664c584e1
Emilio Mariscal <emi.mariscal.dev@gmail.com>
2025-04-24 14:29:33 -0300

    + CORS, ignore OS files

--- tagger/main.py
+++ tagger/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from tagger.api.v1 import tags
 
 app = FastAPI(
     title="My FastAPI App", description="A basic FastAPI application", version="1.0.0"
+)
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Include routers
